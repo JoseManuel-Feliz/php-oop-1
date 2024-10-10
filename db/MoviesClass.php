f9f9a9ac70bb2e37697296f36c45f17edaa21692
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-10 03:32:42 +0200

    added attributes values in movies class

--- db/MoviesClass.php
+++ db/MoviesClass.php
@@ -4,11 +4,47 @@ class movie
     public $title;
     public $date;
     public $poster;
-    public $trama;
+    public $plot;
 };
 
 $matrix = new movie();
+
+//INSTANCE ATTRIBUTES
+$matrix->title = "The matrix";
+$matrix->date = 1999;
+$matrix->poster = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg";
+$matrix->plot = "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.";
+
+
 $scary_movie = new movie();
+
+//INSTANCE ATTRIBUTES
+$scary_movie->title = "Scary Movie";
+$scary_movie->date = 2000;
+$scary_movie->poster = "https://upload.wikimedia.org/wikipedia/en/2/29/Movie_poster_for_%22Scary_Movie%22.jpg";
+$scary_movie->plot = "A year after disposing of the body of a man they accidentally killed, a group of dumb teenagers are stalked by a bumbling serial killer.";
+
+
 $legend = new movie();
-$indiana_jones = new movie();
+
+//INSTANCE ATTRIBUTES
+$legend->title = "I Am Legend";
+$legend->date = 2007;
+$legend->poster = "https://upload.wikimedia.org/wikipedia/en/d/df/I_am_legend_teaser.jpg";
+$legend->plot = "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.";
+
 $jumanji = new movie();
+
+//INSTANCE ATTRIBUTES
+$indiana_jones->title = "Jumanji";
+$indiana_jones->date = 1995;
+$indiana_jones->poster = "https://upload.wikimedia.org/wikipedia/en/b/b6/Jumanji_poster.jpg";
+$indiana_jones->plot = "When two kids find and play a magical board game, they release a man trapped in it for decades - and a host of dangers that can only be stopped by finishing the game.";
+
+$indiana_jones = new movie();
+
+//INSTANCE ATTRIBUTES
+$indiana_jones->title = "Raiders of the Lost Ark";
+$indiana_jones->date = 1981;
+$indiana_jones->poster = "https://upload.wikimedia.org/wikipedia/en/a/a6/Raiders_of_the_Lost_Ark_Theatrical_Poster.jpg";
+$indiana_jones->plot = "In 1936, archaeologists and adventurers of the U.S. government hired Indiana Jones to find the Ark of the Covenant before the Nazis could obtain its extraordinary powers.";
